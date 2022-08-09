165ab4c62edb0bd478fd24b9bbbf9b5980c4c714
Andrew LaRue <pikey6957@gmail.com>
2022-08-09 17:51:23 -0600

    revert

--- app/Components/NameForm.js
+++ app/Components/NameForm.js
@@ -4,16 +4,12 @@ import { ProxyState } from "../AppState.js"
 
 export function getNameForm(name = new Name({})) {
   
-
-  // <input class="${!ProxyState.name.text ? 'img-text' : 'img-text-times'} clear border-0 text-center" type="text" placeholder="Add Name" name="name" id="name" value="${name.name}"></input>
   return `
       <form onsubmit="app.namesController.addName()">
-        
+        <input class="${!ProxyState.name.text ? 'img-text' : 'img-text-times'} clear border-0 text-center" type="text" placeholder="Add Name" name="name" id="name" value="${name.name}">
         <button class="btn border-0">
-        
+        <i class="mdi mdi-gate-or ${!ProxyState.name.text ? 'img-text' : 'img-text-times'} fs-1 selectable" title="Submit"></i>
         </button>
         </form>
   `
-
-  // <i class="mdi mdi-gate-or ${!ProxyState.name.text ? 'img-text' : 'img-text-times'} fs-1 selectable" title="Submit"></i>
 }
