7591ddf6d3515f06c221ec76d8fbc03eca9fd870
Andrew LaRue <pikey6957@gmail.com>
2022-08-09 17:43:36 -0600

    remove ProxyState ref on name model

--- app/Models/Name.js
+++ app/Models/Name.js
@@ -10,10 +10,10 @@ export class Name{
 
   get NameTemplate() {
     return /*html*/ `
-        <h2 class="${!ProxyState.name.text ? 'img-text' : 'img-text-times'}" id="greeting-time"></h2>
-        <h3 class="${!ProxyState.name.text ? 'img-text' : 'img-text-times'} ms-4">
+        <h2 class="${!this.name.text ? 'img-text' : 'img-text-times'}" id="greeting-time"></h2>
+        <h3 class="${!this.name.text ? 'img-text' : 'img-text-times'} ms-4">
           ${this.name}
-          <i onclick="app.namesController.changeName()" class="${!ProxyState.name.text ? 'img-text' : 'img-text-times'} mdi mdi-pencil-circle-outline fs-6  text-danger on-hover selectable" title="Change Name"></i>
+          <i onclick="app.namesController.changeName()" class="${!this.name.text ? 'img-text' : 'img-text-times'} mdi mdi-pencil-circle-outline fs-6  text-danger on-hover selectable" title="Change Name"></i>
         </h3>
   `
 }
